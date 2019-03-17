872ea9111917e9e30161d9ddc41075ed3225cae6
Noweli <ah5noweli@gmail.com>
2019-03-17 20:48:56 +0100

    Project files with BOX2D

--- core/src/com/sup/theprojectgame/TheProjectGame.java
+++ core/src/com/sup/theprojectgame/TheProjectGame.java
@@ -1,38 +1,33 @@
-package com.project.game;
+package com.sup.theprojectgame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.project.game.status.MenuState;
-import com.project.game.status.StateManager;
 
 public class TheProjectGame extends ApplicationAdapter {
-	public static final int GAME_WIDTH = 800;
-	public static final int GAME_HEIGHT = 600;
-	
-	private StateManager state;
-	
 	SpriteBatch batch;
-	
+	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		state = new StateManager();
-		state.push( new MenuState() );
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		state.update(Gdx.graphics.getDeltaTime());
-		state.render(batch);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
+		img.dispose();
 	}
 }
--- desktop/src/com/sup/theprojectgame/desktop/DesktopLauncher.java
+++ desktop/src/com/sup/theprojectgame/desktop/DesktopLauncher.java
@@ -1,17 +1,12 @@
-package com.project.game.desktop;
+package com.sup.theprojectgame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.project.game.TheProjectGame;
+import com.sup.theprojectgame.TheProjectGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		
-		config.width = TheProjectGame.GAME_WIDTH;
-		config.height = TheProjectGame.GAME_HEIGHT;
-		config.title = "The Project Game";
-		
 		new LwjglApplication(new TheProjectGame(), config);
 	}
 }
