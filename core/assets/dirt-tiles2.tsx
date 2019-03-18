99f28c267b11cce7ea482887c290fd450664c468
Crayzol <48689758+Crayzol@users.noreply.github.com>
2019-03-18 18:54:11 +0100

    Tileset

---
+++ core/assets/dirt-tiles2.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="dirt-tiles" tilewidth="8" tileheight="8" tilecount="1536" columns="48">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="../dirt-tiles.png" width="384" height="256"/>
+</tileset>
---
+++ core/assets/terrain_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="terrain_tileset" tilewidth="32" tileheight="32" tilecount="36" columns="9">
+ <image source="../terrain_tileset.png" width="288" height="128"/>
+</tileset>
