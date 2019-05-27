cbc471954661f8ec9ae38890b604defd3a724a19
Crayzol <48689758+Crayzol@users.noreply.github.com>
2019-05-27 23:02:20 +0200

    stage 3

---
+++ core/assets/map/TileKit.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="TileKit" tilewidth="16" tileheight="16" tilecount="912" columns="24">
+ <image source="TileKit.png" width="384" height="608"/>
+</tileset>
